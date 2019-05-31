5c6a7b1b4814237235de49bea4b5a8b9b3c4359a
AndriyMartynyshyn <andriy.martynyshyn@gmail.com>
2019-05-31 15:56:50 +0300

    Updated environment variables

--- models/database.js
+++ models/database.js
@@ -1,14 +1,6 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: '',
-  database: 'koa-passport-example',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-})
+const pool = new Pool() //pool takes default configurations from env variables
 
 module.exports = {
   query: (text, params, callback) => {
